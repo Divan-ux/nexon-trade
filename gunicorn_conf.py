b1b126fd071f4cf36e4db64604f0a370e80e3dd2
Divan <138376667+Divan-ux@users.noreply.github.com>
2026-05-23 08:50:48 +0200

    Update gunicorn_conf.py

--- gunicorn_conf.py
+++ gunicorn_conf.py
@@ -1,6 +1,6 @@
 import os
-
 bind = f"0.0.0.0:{os.environ.get('PORT', '10000')}"
-worker_class = "eventlet"
+worker_class = "eventlet"   # ← asynchronous worker
 workers = 1
 timeout = 120
+worker_hooks = ["server:app"]
